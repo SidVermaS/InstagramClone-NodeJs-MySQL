cdb03d27d451cda34ccc53734c1c6d4becbe6cba
sidverma <siddharthvermas123456@gmail.com>
2020-06-14 19:20:46 +0530

    add

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from './app'
 
 const PORT=process.env.PORT || 3000
 
-const server=http.createServer(PORT)
+const server=http.createServer(app)
 
 server.listen(PORT, ()=>    {
     console.log(`Server started on ${PORT}`)
