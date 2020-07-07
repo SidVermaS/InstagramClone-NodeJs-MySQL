b38a45d52facfcf49400280e93cb0a88591e0379
sidverma <siddharthvermas123456@gmail.com>
2020-07-07 13:56:13 +0530

    add

--- seeders/20200614130001-users.js
+++ seeders/20200614130001-users.js
@@ -18,7 +18,7 @@ module.exports = {
         name: 'Akash Oke',
         role: 'Member',
         photo_url: 'img1.jpg',
-        mobile_no: 9123456789,
+        mobile_no: 9123456781,
         password: 'e10adc3949ba59abbe56e057f20f883e',   
         updatedAt: "2020-05-29T02:48:51.030Z",
         createdAt: "2020-05-29T02:48:51.030Z"    
@@ -28,7 +28,7 @@ module.exports = {
         name: 'Kartik Dake',
         role: 'Member',
         photo_url: 'img2.jpg',
-        mobile_no: 9123456788,
+        mobile_no: 9123456782,
         password: 'e10adc3949ba59abbe56e057f20f883e',   
         updatedAt: "2020-05-29T02:48:51.030Z",
         createdAt: "2020-05-29T02:48:51.030Z"    
@@ -38,7 +38,7 @@ module.exports = {
         name: 'Ketaki Joshi',
         role: 'Member',
         photo_url: 'img3.jpg',
-        mobile_no: 9123456787,
+        mobile_no: 9123456783,
         password: 'e10adc3949ba59abbe56e057f20f883e',   
         updatedAt: "2020-05-29T02:48:51.030Z",
         createdAt: "2020-05-29T02:48:51.030Z"    
@@ -48,7 +48,37 @@ module.exports = {
         name: 'Omkar Kale',
         role: 'Member',
         photo_url: 'img4.jpg',
+        mobile_no: 9123456784,
+        password: 'e10adc3949ba59abbe56e057f20f883e',   
+        updatedAt: "2020-05-29T02:48:51.030Z",
+        createdAt: "2020-05-29T02:48:51.030Z"    
+      },
+      {
+        user_id: 5,
+        name: 'Lokesh Patel',
+        role: 'Member',
+        photo_url: 'img5.jpg',
+        mobile_no: 9123456785,
+        password: 'e10adc3949ba59abbe56e057f20f883e',   
+        updatedAt: "2020-05-29T02:48:51.030Z",
+        createdAt: "2020-05-29T02:48:51.030Z"    
+      },
+      {
+        user_id: 6,
+        name: 'Daisy Mane',
+        role: 'Member',
+        photo_url: 'img6.jpg',
         mobile_no: 9123456786,
+        password: 'e10adc3949ba59abbe56e057f20f883e',   
+        updatedAt: "2020-05-29T02:48:51.030Z",
+        createdAt: "2020-05-29T02:48:51.030Z"    
+      },
+      {
+        user_id: 7,
+        name: 'Vijay Shah',
+        role: 'Member',
+        photo_url: 'img7.jpg',
+        mobile_no: 9123456787,
         password: 'e10adc3949ba59abbe56e057f20f883e',   
         updatedAt: "2020-05-29T02:48:51.030Z",
         createdAt: "2020-05-29T02:48:51.030Z"    
